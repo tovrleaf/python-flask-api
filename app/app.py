044f8f14e1fb80dc2539f94f9063e309923aef71
Niko Kivelä <niko.kivela@verkkokauppa.com>
2016-12-28 01:50:21 +0200

    Skeletons for bare API

--- app/app.py
+++ app/app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello world!'
+article = {
+    'id': 1,
+    'title': 'Hello world!',
+    'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+}
+
+# Retrieve a list of articles
+@app.route('/article', methods = ['GET'])
+def list_articles():
+    return '{}';
+
+# Retrieve article
+@app.route('/article/<int:articleId>', methods = ['GET'])
+def get_article(articleId):
+    return jsonify([article]);
+
+# Create article
+@app.route('/article', methods = ['POST'])
+def add_article():
+    return 'create';
+
+# Modify article
+@app.route('/article/<int:articleId>', methods = ['PUT'])
+def mod_article(articleId):
+    return 'update';
+
+@app.route('/article/<int:articleId>', methods = ['DELETE'])
+def del_article(articleId):
+    return 'del';
 
 
 if __name__ == '__main__':
--- test/run-tests.js
+++ test/run-tests.js
@@ -4,8 +4,70 @@ supertest = require('supertest'),
 api = supertest('http://front');
 
 describe('API', function () {
-  it('should return a 200 response', function(done) {
-    api.get('/')
-      .expect(200, done);
+  it('should be able to delete the whole catalog', function(done) {
+      // FOR TESTING PURPOSES ONLY to run test from same set-up without using fixtures
+      done();
   });
+
+    /*
+  it('should get empty list of articles', function(done) {
+    api.get('/article').end(function(err, res) {
+      expect(res.statusCode).to.equal(200);
+      expect(res.text).to.equal('{}');
+      done();
+    });
+  });
+
+
+  it('should be able to add an article', function(done) {
+    api.post('/article')
+    .send({content: 'foo'}).end(function(err, res) {
+      expect(res.statusCode).to.equal(200);
+      expect(res.text).to.equal('foo');
+      done();
+    });
+  });
+
+  it('should be not add article with false data', function(done) {
+      done();
+  });
+
+  it('should be able to get list of articles containing one item', function(done) {
+      done();
+  });
+
+  it('should be able to read article', function(done) {
+      api.get('/article/1').end(function(err, res) {
+          expect(res.statusCode).to.equal(200);
+          expect(res.headers['content-type']).to.equal('application/json');
+          //expect(res.text).to.equal('1');
+          done();
+      });
+  });
+
+  it('should not find non-existent article', function(done) {
+      done();
+  });
+
+  it('should be able to modify newly added article and return changed data', function(done) {
+      done();
+  });
+
+  it('should not modify article with same data already existing', function(done) {
+      done();
+  });
+
+  it('should not be able to delete article that does not exist', function(done) {
+      done();
+  });
+
+  it('should be able to delete article', function(done) {
+      done();
+  });
+
+  it('should get empty list of articles', function(done) {
+      done();
+  });
+    */
+
 });
